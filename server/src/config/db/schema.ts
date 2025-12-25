19eaeb9b635313e0e4ff1d1d85b81b48f486e460
UBU11 <uvaisubu11@gmail.com>
2025-12-25 14:44:25 +0530

    feat: url db scheme is created

--- server/src/config/db/schema.ts
+++ server/src/config/db/schema.ts
@@ -1,13 +1,20 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-import { neon } from "@neondatabase/serverless"
-import dotenv from "dotenv"
-dotenv.config()
+import { integer, pgTable, varchar,bigint, timestamp } from "drizzle-orm/pg-core";
+import dotenv from "dotenv";
+dotenv.config();
 
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({length:255}).notNull(),
+  name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
-  email: varchar({length:255}).notNull().unique(),
+  email: varchar({ length: 255 }).notNull().unique(),
 });
 
+export const urlsTable = pgTable("urls", {
+  urlId: bigint({mode:'bigint'}).primaryKey().generatedByDefaultAsIdentity().notNull(),
+  userId: integer("user_id").references(() => usersTable.id),
+  originalUrl: varchar({ length: 2048 }).notNull(),
+  updated_at: timestamp(),
+  created_at: timestamp().defaultNow().notNull(),
+  deleted_at: timestamp(),
+});
