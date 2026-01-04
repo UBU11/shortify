769a8ecc80eacf8035ffbf6e85e9830084a0275e
UBU11 <uvaisubu11@gmail.com>
2026-01-04 21:57:27 +0530

    feat:background effect init

---
+++ client/src/pages/Hero.tsx
@@ -0,0 +1,20 @@
+
+import Dither from "../components/Back-Ground/heroSectionBG"
+export default function Hero() {
+  return (
+
+<div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+  <Dither
+    waveColor={[0.5, 0.5, 0.5]}
+    disableAnimation={false}
+    enableMouseInteraction={true}
+    mouseRadius={0.3}
+    colorNum={4}
+    waveAmplitude={0.3}
+    waveFrequency={3}
+    waveSpeed={0.05}
+  />
+</div>
+  )
+}
+
